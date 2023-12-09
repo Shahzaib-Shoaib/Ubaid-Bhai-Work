b4d917a1d7ec91bef02871f848ac1291922241ee
Shahzaib Shoaib <82258192+Shahzaib-Shoaib@users.noreply.github.com>
2023-12-10 04:11:00 +0500

    Update Search.tsx

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -17,7 +17,7 @@ const Search = () => {
   const [searching, setSearching] = useState("");
   const [searchedData, setSearchedData] = useState({
     id: "00",
-    name: "Please type to Search.",
+    name: "",
     href: "#",
   });
 
@@ -39,7 +39,7 @@ const Search = () => {
     <CustomDialog
       contentClassName="dialog-box max-w-[95%] flex flex-col justify-center items-center !bg-transparent !bg-opacity-30 !border-none !shadow-none"
       trigger={
-        <div className="rounded-full p-2 ml-2 border-2 2xl:border-[3px] border-white bg-transparent hover:bg-white hover:border-golden group cursor-pointer">
+        <div className="rounded-full p-2 ml-2 border-2 2xl:border-[3px] bg-transparent   group cursor-pointer">
           <Icons.search className="text-white w-4 h-4 group-hover:text-golden " />
         </div>
       }
@@ -48,28 +48,36 @@ const Search = () => {
           <CustomForm form={form} onSubmit={form.handleSubmit(onSubmit)}>
             <div className="flex gap-5 sm:gap-2 justify-center items-center">
               <CustomInput
-                className="h-14 sm:h-10 border-white border-2"
+                className="h-14 sm:h-10  border-2"
                 control={form.control}
                 label="Search"
                 name="name"
                 description="Search"
                 placeholder="Search Services"
-                fieldclass="!w-[60%] !text-black border-golden"
+                fieldclass="!w-[60%] !text-black "
               />
-              <CustomButton type="submit" variant={"light"} className="h-14 sm:h-10">
+              <CustomButton
+                type="submit"
+                variant={"dark"}
+                className="h-14 sm:h-10"
+              >
                 Search
               </CustomButton>
             </div>
           </CustomForm>
           <div className="data py-4 text-center">
-            {searchedData.name === "Please type to Search." ? (
-              <p className="text-white !text-center text-[32px] sm:text-[22px] font-bold">{searchedData.name}</p>
+            {searchedData.name === "" ? (
+              <p className="text-white !text-center text-[32px] sm:text-[22px] font-bold">
+                {searchedData.name}
+              </p>
             ) : (
               <Link href={searchedData.href}>
-                <p className="text-black underline !text-center text-[32px] sm:text-[22px] font-bold "> {searchedData.name}</p>
+                <p className="text-black underline !text-center text-[32px] sm:text-[22px] font-bold ">
+                  {" "}
+                  {searchedData.name}
+                </p>
               </Link>
-            )
-            }
+            )}
           </div>
         </div>
       }
